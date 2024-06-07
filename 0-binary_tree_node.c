1113d7823766d9442c6078173d222580d623c484
Onyinye-Judith <onyinyejudith55@gmail.com>
2024-06-07 13:16:40 +0100

    Binary-trees

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,12 +1,11 @@
 #include "binary_trees.h"
 
-/**
+/*
  * binary_tree_node - Create a binary node
  * @parent: node parent
  * @value: Value of the node
  * Return: the new node
  */
-
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
